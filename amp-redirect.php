70a70938f8efec132f85db379c13cb7a6da83b09
JesusDF <jesusdf@gmail.com>
2017-02-11 13:45:36 +0100

    Check if AMP is enabled.

--- amp-redirect.php
+++ amp-redirect.php
@@ -1,6 +1,8 @@
 <?php
 
-if (is_single()) {
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+if (is_plugin_active('amp/amp.php') && is_single()) {
     global $wp;
     $mobile_match = '/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/';
     $current_url = home_url(add_query_arg(array(),$wp->request));
